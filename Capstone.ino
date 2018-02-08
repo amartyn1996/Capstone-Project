b6e8a835de5b76b3e027630774c012cca49115bc
amartyn1996 <amartyn1996@gmail.com>
2018-02-08 13:37:51 -0500

    Can now receive PWM signals from the Remote Controller.
    Set up an Interrupt Service Routine to detect pulse rise and fall from pins 8-11.
    Pin 10 (Pitch) pulse length is always 2000us. I believe this is caused by a problem in remote controller and not my code.

--- Capstone.ino
+++ Capstone.ino
@@ -14,6 +14,16 @@ ESC* esc;
 OrientationHandler* orHand;
 float throttle1=0, throttle2=0, throttle3=0, throttle4=0;
 
+volatile uint8_t pinsState = 0;
+volatile uint32_t startTimeOnPin8 = 0;
+volatile uint32_t startTimeOnPin9 = 0;
+volatile uint32_t startTimeOnPin10 = 0;
+volatile uint32_t startTimeOnPin11 = 0;
+volatile uint32_t timeOnPin8 = 0;
+volatile uint32_t timeOnPin9 = 0;
+volatile uint32_t timeOnPin10 = 0;
+volatile uint32_t timeOnPin11 = 0;
+
 void setup() {
   
   while (SAFEMODE)
@@ -22,18 +32,22 @@ void setup() {
   DDRD |= 0b11110000;
   PORTD |= 0b11110000;
 
+  DDRB &= 0b11110000; //Set pins 8-11 to input.
+  PCICR |= (1<<PCIE0); //Enable pin change interupts on PortB.
+  PCMSK0 |= (0xF<<PCINT8); //Enagle pin change interupts on pins 8-11.
+
   esc = new ESC(4000);
   imu = new MPU6050();
   orHand = new OrientationHandler(imu);
     
   Serial.begin(9600);
 
-  Serial.println("Begin ESC Calibrate");
-  esc->calibrate();
-  Serial.println("End ESC Calibrate");
+  //Serial.println("Begin ESC Calibrate");
+  //esc->calibrate();
+  //Serial.println("End ESC Calibrate");
     
-  orHand->initialize();
-  orHand->calibrate();
+  //orHand->initialize();
+  //orHand->calibrate();
   
   #if VISUALIZE
     visualize();
@@ -43,14 +57,21 @@ void setup() {
 }
 
 void loop() {
-  static uint32_t lastCycleTime = micros();
-  static float pitch = 0;
-  static float roll = 0;
+  //static uint32_t lastCycleTime = micros();
+  //static float pitch = 0;
+  //static float roll = 0;
   
-  orHand->calcOrientation(pitch, roll);
+  //orHand->calcOrientation(pitch, roll);
   
-  esc->demandControl(-pitch/90, -roll/90, .25, lastCycleTime);
-  
+  //esc->demandControl(-pitch/90, -roll/90, .25, lastCycleTime);
+  static int iterator = 0;
+  if (iterator % 10000 == 0) {
+    Serial.print(" Pin8 : "); Serial.print(timeOnPin8);
+    Serial.print(" Pin9 : "); Serial.print(timeOnPin9);
+    Serial.print(" PinA : "); Serial.print(timeOnPin10);
+    Serial.print(" PinB : "); Serial.println(timeOnPin11);
+  }
+  iterator++;
 }
 
 /**
@@ -79,3 +100,34 @@ void visualize() {
     }
 }
 
+/*
+ *  Interrupt Service Routine for pin changes on PORTB.
+ *  Measures the time between pulses coming from the RF receiver.
+ * 
+ *  NOTE:
+ *  PIN 8  is roll.
+ *  PIN 9  is throttle.
+ *  PIN 10 is pitch.
+ *  PIN 11 is yaw.
+ */
+ISR(PCINT0_vect) {
+  uint8_t curPinState = PINB;
+  uint8_t prevPinState = pinsState;
+  uint8_t changedPins = curPinState ^ prevPinState;
+  uint8_t onlyChangedPins = curPinState & changedPins;
+  uint32_t currentTime = micros();
+
+  //If the pin turned high, set the starting time to the current time.
+  startTimeOnPin8  = (onlyChangedPins & 0b00000001) ? currentTime : startTimeOnPin8;
+  startTimeOnPin9  = (onlyChangedPins & 0b00000010) ? currentTime : startTimeOnPin9;
+  startTimeOnPin10 = (onlyChangedPins & 0b00000100) ? currentTime : startTimeOnPin10;
+  startTimeOnPin11 = (onlyChangedPins & 0b00001000) ? currentTime : startTimeOnPin11;
+
+  //If the pin turned low, we now know the length of the pulse.
+  timeOnPin8   = ((changedPins & 0x1) && !(curPinState & 0x1)) ? (currentTime - startTimeOnPin8)  : timeOnPin8;
+  timeOnPin9   = ((changedPins & 0x2) && !(curPinState & 0x2)) ? (currentTime - startTimeOnPin9)  : timeOnPin9;
+  timeOnPin10  = ((changedPins & 0x4) && !(curPinState & 0x4)) ? (currentTime - startTimeOnPin10) : timeOnPin10;
+  timeOnPin11  = ((changedPins & 0x8) && !(curPinState & 0x8)) ? (currentTime - startTimeOnPin11) : timeOnPin11;
+
+  pinsState = curPinState;
+}
