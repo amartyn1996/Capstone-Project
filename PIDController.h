ff31bc2cedf6fc9f889a037fd1573d14252843b3
amartyn1996 <amartyn1996@gmail.com>
2018-04-02 15:08:21 -0400

    Finally repaired the motor mounting. Back to PID tuning...

--- PIDController.h
+++ PIDController.h
@@ -1,13 +1,13 @@
 #ifndef PIDController_H
 #define PIDController_H
 
-#define PID_P_GAIN_P 0.5
+#define PID_P_GAIN_P 0.2
 #define PID_I_GAIN_P 0.00
 #define PID_D_GAIN_P 100.0
-#define PID_P_GAIN_R 0.5
+#define PID_P_GAIN_R 0.2
 #define PID_I_GAIN_R 0.00
 #define PID_D_GAIN_R 100.0
-#define PID_P_GAIN_Y 0.5
+#define PID_P_GAIN_Y 0.2
 #define PID_I_GAIN_Y 0.00
 #define PID_D_GAIN_Y 100.0
 #define NUM_ELEMENTS_TO_AVERAGE 9
