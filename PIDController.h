4f394bc078b3776ab0537267c59c5287322d1344
amartyn1996 <amartyn1996@gmail.com>
2018-04-10 12:51:51 -0400

    More tuning. Got some good flights in
    I can tell I am approaching a good tune. Seems like the only thing keeping me from flying longer is the size of area I am flying it in.

--- PIDController.h
+++ PIDController.h
@@ -1,15 +1,15 @@
 #ifndef PIDController_H
 #define PIDController_H
 
-#define PID_P_GAIN_P 0.10
-#define PID_I_GAIN_P 2.50
-#define PID_D_GAIN_P 150.0
-#define PID_P_GAIN_R 0.10
-#define PID_I_GAIN_R 2.50
-#define PID_D_GAIN_R 150.0
-#define PID_P_GAIN_Y 0.10
-#define PID_I_GAIN_Y 2.50
-#define PID_D_GAIN_Y 150.0
+#define PID_P_GAIN_P 0.07
+#define PID_I_GAIN_P 2.25
+#define PID_D_GAIN_P 200.0
+#define PID_P_GAIN_R 0.07
+#define PID_I_GAIN_R 2.25
+#define PID_D_GAIN_R 200.0
+#define PID_P_GAIN_Y 0.07
+#define PID_I_GAIN_Y 2.25
+#define PID_D_GAIN_Y 200.0
 #define NUM_ELEMENTS_TO_AVERAGE 9
 #define EXPECTED_DELTA_TIME 4000.0
 #define MAX_SUM_ERROR 0.02
